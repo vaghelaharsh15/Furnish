95c456365764a907ce9ad1460d1dd7f94f1b6138
AbhirajsinhDabhi <abhirajsinhdabhi07@gmail.com>
2026-03-24 13:32:34 +0530

    Updated contact page and structure

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "myapp"
+    "myapp",
 ]
 
 MIDDLEWARE = [
@@ -115,4 +115,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
